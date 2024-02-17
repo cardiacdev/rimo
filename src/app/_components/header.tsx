3454e828ceadb8ae43a2df39620fbfe5c57d6e45
Mark Loose <m.loose@boewa.de>
2024-02-17 19:13:26 +0100

    Add page header

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,29 @@
+import { getServerAuthSession } from "~/server/auth";
+import { ModeToggle } from "./mode-toggle";
+import Link from "next/link";
+
+export const Header = async () => {
+  const session = await getServerAuthSession();
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <p>
+          <span className="text-2xl font-bold">RI</span>
+          <span className="text-2xl font-bold text-primary">MO</span>
+        </p>
+        <div className="flex flex-1 items-center justify-end space-x-4 ">
+          <nav className="flex items-center space-x-4">
+            <Link
+              href={session ? "/api/auth/signout" : "/api/auth/signin"}
+              className="rounded-full bg-primary px-4 py-1 font-semibold no-underline transition hover:bg-secondary"
+            >
+              {session ? "Sign out" : "Sign in"}
+            </Link>
+            <ModeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
