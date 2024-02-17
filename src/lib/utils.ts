39d11b7f96d05f1d28b4dc9b332b5a6876f39b27
Mark Loose <m.loose@boewa.de>
2024-02-17 19:13:38 +0100

    Add utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export type SearchParams = ConstructorParameters<typeof URLSearchParams>[0];
+
+export function URLSearchParamsToObj(searchParams: SearchParams) {
+  if (!searchParams) return {};
+
+  if (typeof searchParams === "string") {
+    return Object.fromEntries(new URLSearchParams(searchParams).entries());
+  }
+
+  if (Array.isArray(searchParams)) {
+    return Object.fromEntries(searchParams) as Record<string, string>;
+  }
+
+  if (searchParams instanceof URLSearchParams) {
+    return Object.fromEntries(searchParams.entries());
+  }
+
+  return searchParams;
+}
