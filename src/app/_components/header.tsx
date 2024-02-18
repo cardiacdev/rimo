96e1802389d31134083522cb724b643e7ac9aa5b
Mark Loose <m.loose@boewa.de>
2024-02-18 19:56:42 +0100

    Add home link to brand logo in nav

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -8,10 +8,10 @@ export const Header = async () => {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <p>
+        <Link href={"/"}>
           <span className="text-2xl font-bold">RI</span>
           <span className="text-2xl font-bold text-primary">MO</span>
-        </p>
+        </Link>
         <div className="flex flex-1 items-center justify-end space-x-4 ">
           <nav className="flex items-center space-x-4">
             <Link
