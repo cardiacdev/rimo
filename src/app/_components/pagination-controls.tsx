f9a9d1b57b59701c88af245c921d54b1b76d4a28
Mark Loose <m.loose@boewa.de>
2024-02-18 15:44:41 +0100

    Create pagination controls

---
+++ src/app/_components/pagination-controls.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  CaretLeftIcon,
+  CaretRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { Button } from "~/components/ui/button";
+
+interface PaginationControlsProps {
+  page: number;
+  pageCount: number;
+  first: () => void;
+  previous: () => void;
+  next: () => void;
+  last: () => void;
+}
+
+export const PaginationControls = ({
+  page,
+  pageCount,
+  first,
+  previous,
+  next,
+  last,
+}: PaginationControlsProps) => {
+  return (
+    <div className="absolute inset-x-0 z-50 flex items-center justify-center space-x-2 py-4">
+      <Button variant="outline" size="sm" onClick={first} disabled={page === 1}>
+        <DoubleArrowLeftIcon />
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={previous}
+        disabled={page === 1}
+      >
+        <CaretLeftIcon />
+      </Button>
+      <span>{`Page ${page} of ${pageCount}`}</span>
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={next}
+        disabled={page === pageCount}
+      >
+        <CaretRightIcon />
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={last}
+        disabled={page === pageCount}
+      >
+        <DoubleArrowRightIcon />
+      </Button>
+    </div>
+  );
+};
