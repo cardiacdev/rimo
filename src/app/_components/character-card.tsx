a2c7b45c8fb9fdc8ac0f639b3ebeae2028179739
Mark Loose <m.loose@boewa.de>
2024-02-18 18:34:50 +0100

    Add favorite buttons for mutations

--- src/app/_components/character-card.tsx
+++ src/app/_components/character-card.tsx
@@ -5,9 +5,37 @@ import { Card } from "~/components/ui/card";
 import { type Character } from "../schema/character";
 import { useModal } from "@ebay/nice-modal-react";
 import { CharacterDetailDialog } from "./character-detail-dialog";
+import { useSession } from "next-auth/react";
+import { api } from "~/trpc/react";
+import { Heart } from "lucide-react";
 
 export const CharacterCard = ({ character }: { character: Character }) => {
   const modal = useModal(CharacterDetailDialog, { character });
+
+  const { data } = useSession();
+  const { data: favorites } = api.character.getFavoriteCharacters.useQuery({
+    userId: data?.user.id ?? "",
+  });
+
+  const utils = api.useUtils();
+
+  const favoritize = api.character.addFavoriteCharacter.useMutation({
+    onSuccess: async () => {
+      // TODO - invalidate based on query keys
+      await utils.invalidate();
+    },
+  });
+
+  const unfavoritize = api.character.removeFavoriteCharacter.useMutation({
+    onSuccess: async () => {
+      // TODO - invalidate based on query keys
+      await utils.invalidate();
+    },
+  });
+
+  const isFavorite = favorites?.some(
+    (fav) => fav.characterId === String(character.id),
+  );
 
   return (
     <Card key={character.id} className="mt-4 flex items-center gap-4 p-4">
@@ -28,6 +56,30 @@ export const CharacterCard = ({ character }: { character: Character }) => {
         height={75}
         className="rounded-full"
       />
+      {isFavorite && (
+        <Heart
+          className="mt-auto h-6 w-6 cursor-pointer self-end fill-red-500 text-red-500"
+          onClick={() => {
+            data?.user.id &&
+              unfavoritize.mutate({
+                userId: data.user.id,
+                characterId: String(character.id),
+              });
+          }}
+        />
+      )}
+      {!isFavorite && (
+        <Heart
+          className="mt-auto h-6 w-6 cursor-pointer self-end"
+          onClick={() =>
+            data?.user.id &&
+            favoritize.mutate({
+              userId: data.user.id,
+              characterId: String(character.id),
+            })
+          }
+        />
+      )}
     </Card>
   );
 };
--- src/app/_components/episode-card.tsx
+++ src/app/_components/episode-card.tsx
@@ -1,24 +1,74 @@
+"use client";
+
 import { Card } from "~/components/ui/card";
 import { type Episode } from "../schema/episode";
 import { useModal } from "@ebay/nice-modal-react";
 import { EpisodeDetailDialog } from "./episode-detail-dialog";
+import { api } from "~/trpc/react";
+import { useSession } from "next-auth/react";
+import { Heart } from "lucide-react";
 
 export const EpisodeCard = ({ episode }: { episode: Episode }) => {
   const modal = useModal(EpisodeDetailDialog, { episode });
+  const { data } = useSession();
+  const { data: favorites } = api.episode.getFavoriteEpisodes.useQuery({
+    userId: data?.user.id ?? "",
+  });
+
+  const utils = api.useUtils();
+
+  const favoritize = api.episode.addFavoriteEpisode.useMutation({
+    onSuccess: async () => {
+      // TODO - invalidate based on query keys
+      await utils.invalidate();
+    },
+  });
+
+  const unfavoritize = api.episode.removeFavoriteEpisode.useMutation({
+    onSuccess: async () => {
+      // TODO - invalidate based on query keys
+      await utils.invalidate();
+    },
+  });
+
+  const isFavorite = favorites?.some(
+    (fav) => fav.episodeId === String(episode.id),
+  );
 
   return (
     <Card key={episode.id} className="mt-4 flex min-w-52 flex-col gap-2 p-4">
       <button
-        className="max-w-40 text-left text-xl font-bold"
+        className="max-w-48 text-left text-lg font-bold"
         type="button"
         onClick={() => modal.show({ episode })}
       >
         {episode.name}
       </button>
       <p>{episode.episode}</p>
-      <p className="">
-        <i>{episode.air_date}</i>
-      </p>
+      {isFavorite && (
+        <Heart
+          className="mt-auto h-6 w-6 cursor-pointer self-end fill-red-500 text-red-500"
+          onClick={() => {
+            data?.user.id &&
+              unfavoritize.mutate({
+                userId: data.user.id,
+                episodeId: String(episode.id),
+              });
+          }}
+        />
+      )}
+      {!isFavorite && (
+        <Heart
+          className="mt-auto h-6 w-6 cursor-pointer self-end"
+          onClick={() =>
+            data?.user.id &&
+            favoritize.mutate({
+              userId: data.user.id,
+              episodeId: String(episode.id),
+            })
+          }
+        />
+      )}
     </Card>
   );
 };
