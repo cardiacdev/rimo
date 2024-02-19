372d231447bdc1eae6d62c0e8717bc4347cd923a
Mark Loose <m.loose@boewa.de>
2024-02-19 21:09:23 +0100

    Hide AI functionalities for unauthorized users

--- src/app/episode/[id]/_components/episode-details.tsx
+++ src/app/episode/[id]/_components/episode-details.tsx
@@ -4,12 +4,14 @@ import { CharactersForEpisode } from "./characters-for-episode";
 import { Suspense } from "react";
 import { EpisodeHeart } from "../../_components/episode-heart";
 import { EpisodeFunFacts } from "./episode-fun-facts";
+import { getServerAuthSession } from "~/server/auth";
 
 interface EpisodeDetailsProps {
   episode: Episode;
 }
 
-export const EpisodeDetails = ({ episode }: EpisodeDetailsProps) => {
+export const EpisodeDetails = async ({ episode }: EpisodeDetailsProps) => {
+  const session = await getServerAuthSession();
   return (
     <>
       <div className="text-md flex flex-col gap-2">
@@ -23,7 +25,7 @@ export const EpisodeDetails = ({ episode }: EpisodeDetailsProps) => {
         <p>
           <b>Created</b>: {format(episode.created, "MM/dd/yyyy")}
         </p>
-        <EpisodeFunFacts episode={episode} />
+        {session?.user.id && <EpisodeFunFacts episode={episode} />}
         <Suspense fallback={<div>Loading...</div>}>
           <CharactersForEpisode episode={episode} />
         </Suspense>
