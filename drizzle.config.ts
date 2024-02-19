9b2c496387c283beda25c7c4547636b0b757ef77
Mark Loose <m.loose@boewa.de>
2024-02-19 20:34:25 +0100

    Switch to turso db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,10 @@ import { env } from "~/env.js";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  driver: "better-sqlite",
+  driver: "turso",
   dbCredentials: {
     url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
   tablesFilter: ["rimo_*"],
 } satisfies Config;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,13 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "~/env.js";
 import * as schema from "./schema";
 
-export const db = drizzle(
-  new Database(env.DATABASE_URL, {
-    fileMustExist: false,
-  }),
-  { schema },
-);
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const client = createClient({
+  url: env.DATABASE_URL,
+  authToken: env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle(client, { schema });
